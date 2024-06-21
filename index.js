43b34e119d51c2e0cd46a7ff6addf8b6de5f79ab
raghav369r <raghav1010reddy@gmail.com>
2024-06-22 01:20:42 +0530

    connected to mongoDB atlas and created All required models and relations

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
 const cors = require("cors");
+require("dotenv").config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 const typeDefs = gql`
   type Query {
     hello: String
@@ -24,7 +25,7 @@ const server = new ApolloServer({
 const app = express();
 app.use(cors());
 server.start().then(() => {
-  server.applyMiddleware({ app, path: "/graphql" });
+  server.applyMiddleware({ app, path: "/graphql", cors: false });
 
   app.listen({ port: PORT }, () =>
     console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`)
